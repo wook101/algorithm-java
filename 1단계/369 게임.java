77e1167d32e89d2f4caa06d1b7e169ff1bbfdda8
wook101 <ehddnr1022@gmail.com>
2021-10-19 21:42:59 +0900

    Create 369 게임.java

---
+++ 1단계/369 게임.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		System.out.print(solution(input));
+	}
+	
+  
+	public static int solution(String input){
+		int n = Integer.parseInt(input);
+		int count = 0;
+		for(int num=1;num<n;num++){
+				String st_num = String.valueOf(num);
+			for(int i=0;i<st_num.length();i++){
+						if (st_num.charAt(i)=='0') 
+							continue;
+						if (Character.getNumericValue(st_num.charAt(i))%3==0)
+							count++;
+					}
+			}
+			return count;
+		}
+		
+}
